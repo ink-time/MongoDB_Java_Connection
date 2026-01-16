3068738c1ea03b2ed4a420b3f5e4c0df9b2da675
InkTime <a.aguirreponzano@gmail.com>
2026-01-16 14:05:15 +0100

    Creating project 1

--- src/main/java/LeerClientJson.java
+++ src/main/java/LeerClientJson.java
@@ -1,2 +1,17 @@
-public class LeerClientJson {
+import com.google.gson.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;public class LeerClientJson {
+    public static void main(String[] args) {
+
+        try {
+            Gson gson = new Gson();
+            // Aquí ocurre la deserialización: el archivo JSON se transforma en un objeto manejable desde Java.
+            JsonObject cliente = gson.fromJson(
+                    new FileReader("cliente.json"), JsonObject.class
+            );
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
