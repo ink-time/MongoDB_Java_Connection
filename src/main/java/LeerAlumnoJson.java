d88d91f3cfb79fa5a5adca5812599e596c3e965c
InkTime <a.aguirreponzano@gmail.com>
2026-01-27 09:28:47 +0100

    Update LeerAlumnoJson.java

--- src/main/java/LeerAlumnoJson.java
+++ src/main/java/LeerAlumnoJson.java
@@ -3,25 +3,28 @@ import com.google.gson.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 public class LeerAlumnoJson {
-    public static void leerJsonFile(){
+//    public static void leerJsonFile(){
+static void main(String[] args) {
 
-        try {
+
+     try {
             Gson gson = new Gson();
             // Aquí ocurre la deserialización: el archivo JSON se transforma en un objeto manejable desde Java.
             JsonObject alumno = gson.fromJson(
                     new FileReader("json_files/alumnos.json"), JsonObject.class
             );
 
-            int edad = alumno.get("edad").getAsInt();
+
 
             String nombre = alumno.get("nombre").getAsString();
             String email = alumno.get("email").getAsString();
-
+            int edad = alumno.get("edad").getAsInt();
             boolean activo = alumno.get("activo").getAsBoolean();
             double saldo = alumno.get("saldo").getAsDouble();
             String telefono = alumno.get("telefono").getAsString();
             String pais = alumno.get("pais").getAsString();
 
+            // If I had an array of phones this would be necessary:
 //            JsonArray telefonos = alumno.getAsJsonArray("telefonos");
 //
 //            for (JsonElement t : telefonos) {
