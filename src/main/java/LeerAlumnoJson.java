21a4b6f62198c8ccd49f777e91bad40c99e5e293
InkTime <a.aguirreponzano@gmail.com>
2026-01-27 09:18:50 +0100

    Gson-json update

--- src/main/java/LeerAlumnoJson.java
+++ src/main/java/LeerAlumnoJson.java
@@ -3,7 +3,7 @@ import com.google.gson.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 public class LeerAlumnoJson {
-    public static void main(String[] args) {
+    public static void leerJsonFile(){
 
         try {
             Gson gson = new Gson();
